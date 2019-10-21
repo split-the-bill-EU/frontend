ea0097775190cbbf82cb85b5339fbfc594c1f972
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-21 16:44:42 +0100

    Delete nested frontend folder

--- frontend/src/state/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import countReducer from './counterReducer';
-
-const rootReducer = combineReducers({
-    test: countReducer,
-});
-
-export default rootReducer;
