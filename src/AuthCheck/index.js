efa51a9cd8ff34fdec30fdea05341d78681f88e3
unknown <neduvinci@gmail.com>
2019-10-22 14:06:37 +0100

    Created Layout, Dashboard and AllBills folders with corresponding index files

--- src/AuthCheck/index.js
+++ src/AuthCheck/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-  export default function withAuthCheck(Component, props) {
-    if (localStorage.getItem('token')) {
-      return <Component {...props} />;
-    }
-    return <Redirect to='/login' />;
-  };
+export default function withAuthCheck(Component, props) {
+  if (localStorage.getItem('token')) {
+    return <Component {...props} />;
+  }
+  return <Redirect to='/login' />;
+};
