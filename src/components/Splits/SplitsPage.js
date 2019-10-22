fcb6f606b1cd6454589f4a9fffc7aa3855c175af
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-22 21:08:54 +0100

    On submit of the form, auth post request should be sent to backend, updating the database

--- src/components/Splits/SplitsPage.js
+++ src/components/Splits/SplitsPage.js
@@ -1,34 +1,86 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { Formik, Form, Field } from 'formik';
 import * as creators from '../../state/actionCreators';
+import AxiosAuth from '../../axios/AxiosAuth';
+
+const initalSplit = ''
+const patchURL = 'https://split-the-bill-api.herokuapp.com/api/splits'
 
 export function SplitsPage (props) {
     const {getSplits, splits} = props;
+    const [editing, setEditing] = useState(false);
+    const [splitToSettle, setSplitToSettle] = useState(initalSplit)
+
+    const settle = (formValues, actions) => {
+        const payload = {amount: formValues.amount}
+
+        AxiosAuth().patch(`${patchURL}/${splitToSettle}/settleUp?au&=`, payload)
+            .then(res => {
+                debugger
+            })
+            .catch(err => {
+                debugger
+            })
+    }
+
+    const settleUp = (id) => {
+        setEditing(true);
+        setSplitToSettle(id)
+    }
 
     useEffect(() => {
         getSplits();
     }, []);
 
+    const styles2 = {
+        margin: '5px',
+        width: '200px',
+        height: '50px',
+        borderRadius: '10px',
+      };
+
     return (
         <div>
-            {
-                splits.map(split => (
-                    <Splits split={split}/>
-                ))
-            }
+            <ul>
+                {
+                    splits.map(split => (
+                        <Splits split={split} settleUp={settleUp}/>
+                    ))
+                }
+            </ul>
+                {
+                    editing && (
+                        <Formik
+                        initialValues={{amount: ''}}
+                        onSubmit={settle}
+                        render={() => (
+                            <Form>
+                              <div>
+                                <Field style={styles2} name='amount' type="text" placeholder='Amount' />
+                              </div>
+                               <div>
+                               <button type='submit'>save</button>
+                               <button onClick={() => setEditing(false)}>cancel</button>
+                              </div>
+                            </Form>
+                        )}
+                      />
+                    )
+                }
         </div>
     )
 }
 
 
 function Splits(props) {
-    const {split} = props;
+    const {split, settleUp} = props;
 
     return (
         <div>
             <div>{split.bill.title}</div>
             <div>{split.amount}</div>
-            {/* <button onClick={() => settleUp(amount)}>Settle</button> */}
+            <button onClick={() => settleUp(split.id)}>Settle</button>
         </div>
     )
 }
