6b7eb5d496b694cf4b11cef18b6776a219f5e24b
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-22 08:17:04 +0100

    Sign up feature complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
-import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
       <Counter/>
-      <Signup/>
     </div>
   );
 }
