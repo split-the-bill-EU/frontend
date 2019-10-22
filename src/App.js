f4ceeee76f90bcb99fc1d25ffb30818a8f639f1a
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-22 22:31:31 +0100

    component styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
-import SplitsPage from './components/Splits/SplitsPage';
 
 function App() {
   return (
     <div className="App">
       <Counter/>
-      <SplitsPage/>
     </div>
   );
 }
--- src/components/Splits/SplitsPage.js
+++ src/components/Splits/SplitsPage.js
@@ -3,6 +3,45 @@ import { connect } from 'react-redux';
 import { Formik, Form, Field } from 'formik';
 import * as creators from '../../state/actionCreators';
 import AxiosAuth from '../../axios/AxiosAuth';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 20%;
+    margin: 1.5em;
+    background-color: #FFB884;
+    box-shadow: 0 16px 16px 0 rgba(0,0,0,0.2);`
+
+const OuterDiv = styled.div`
+    display: flex;
+    background-color: whitesmoke;
+    height: 100vh;
+    flex-basis: 100%;
+    justify-content: space-around;
+    align-items: center;`
+
+const InnerDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-basis: 90%;
+    height: 90%;
+    justify-content: space-between;
+    align-self: center;`
+
+const Button = styled.button`
+    color: white;
+    background-color: #75C22F;
+    text-transform: uppercase;
+    margin: 1.5em;
+    text-align: center;
+    font-size: 1em;
+    font-family: system-ui, sans-serif;
+    border-radius: 0.3em;
+    padding: 0.5em;
+    text-decoration: none;
+    border: 0;
+    cursor: pointer;`
 
 const initalSplit = ''
 const patchURL = 'https://split-the-bill-api.herokuapp.com/api/splits'
@@ -12,7 +51,7 @@ export function SplitsPage (props) {
     const [editing, setEditing] = useState(false);
     const [splitToSettle, setSplitToSettle] = useState(initalSplit)
 
-    const settle = (formValues, actions) => {
+    const settle = (formValues) => {
         const payload = {amount: formValues.amount}
 
         AxiosAuth().patch(`${patchURL}/${splitToSettle}/settleUp?au&=`, payload)
@@ -42,11 +81,14 @@ export function SplitsPage (props) {
         borderRadius: '10px',
       };
 
+    const unpaidBills = splits.filter(split => split.amount > 0);
+
     return (
-        <div>
+        <OuterDiv>
+            <InnerDiv>
             <ul>
                 {
-                    splits.map(split => (
+                    unpaidBills.map(split => (
                         <Splits key={split.id} split={split} settleUp={settleUp}/>
                     ))
                 }
@@ -58,19 +100,21 @@ export function SplitsPage (props) {
                         onSubmit={settle}
                         render={() => (
                             <Form>
+                                <h2>Enter amount to be paid</h2>
                               <div>
                                 <Field style={styles2} name='amount' type="text" placeholder='Amount' />
                               </div>
                                <div>
-                               <button type='submit'>save</button>
-                               <button onClick={() => setEditing(false)}>cancel</button>
+                               <Button type='submit'>save</Button>
+                               <Button onClick={() => setEditing(false)}>cancel</Button>
                               </div>
                             </Form>
                         )}
                       />
                     )
                 }
-        </div>
+            </InnerDiv>
+        </OuterDiv>
     )
 }
 
@@ -79,11 +123,12 @@ function Splits(props) {
     const {split, settleUp} = props;
 
     return (
-        <div>
-            <div>{split.bill.title}</div>
-            <div>{split.amount}</div>
-            <button onClick={() => settleUp(split.id)}>Settle</button>
-        </div>
+        <Card>
+
+            <h2>{split.bill.title}</h2>
+            <div>Amount: {split.amount}</div>
+            <Button onClick={() => settleUp(split.id)}>Settle</Button>
+        </Card>
     )
 }
 
