2eb567739a068d921fb71528ab803871e363a73a
unknown <neduvinci@gmail.com>
2019-10-23 15:04:49 +0100

    Integrated Debit page in App and confirmed rendering. Added a div in SplitsPage to show when you don't have any unpaid bills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import SplitBills from './components/Splits/SplitBills';
+import Debit from './components/Splits/Debit';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import withAuthCheck from './AuthCheck';
@@ -14,7 +14,7 @@ function App() {
       <Route exact path="/" component={Login} />
       <Route path="/signup" component={Signup} />
       <Route path='/dashboard' render={props => withAuthCheck(Dashboard, props)} />
-      <Route path='/my_bills' render={props => withAuthCheck(SplitBills, props)} />
+      <Route path='/my_debit' render={props => withAuthCheck(Debit, props)} />
       <Route path='/create_bill' render={props => withAuthCheck(CreateBill, props)} />
     </div>
   );
---
+++ src/components/Splits/Debit/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../Layout';
+import SplitBills from './SplitsPage';
+
+export default () => {
+    return (
+        <Layout title='My Debit'>
+            <SplitBills />
+        </Layout>
+    )
+}
