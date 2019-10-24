3e1ad4c6b04fb175d819eb0e7517afae43b522ee
unknown <neduvinci@gmail.com>
2019-10-24 07:08:46 +0100

    Added basic layout for dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import clsx from 'clsx';
+import { connect } from 'react-redux'
+import { useStyles } from '../Layout';
+import { Grid, Paper, } from '@material-ui/core';
 
-export default () => {
-    return <div>Dashboard</div>
+export const Dashboard = () => {
+    const classes = useStyles();
+    const styles = {height: '100px', 'background-color': '#FFB884'}
+
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={4}>
+                <Paper className={fixedHeightPaper} style={styles}>
+                Total Balance
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={8} lg={4}>
+              <Paper className={fixedHeightPaper} style={styles}>
+                Owing
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4} lg={4}>
+              <Paper className={fixedHeightPaper} style={styles}>
+                Owed
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                Orders
+              </Paper>
+            </Grid>
+          </Grid>
+    )
 }
+
+export default connect(state => state)(Dashboard);
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -3,6 +3,7 @@ import Layout from '../Layout'
 import Dashboard from './Dashboard';
 
 export default () => {
+
     return (
         <Layout title='Dashboard'>
             <Dashboard />
