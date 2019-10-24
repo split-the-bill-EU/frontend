5f66c2273844405b94673d374c9983ecb3210005
unknown <neduvinci@gmail.com>
2019-10-24 22:27:17 +0100

    Completed set up of dashboard reducer

--- src/state/reducers/dashboardReducer.js
+++ src/state/reducers/dashboardReducer.js
@@ -9,6 +9,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case types.SET_OWING_AMOUNT:
+            return {...state, owing: action.payload}
+        case types.SET_OWED_AMOUNT:
+            return {...state, owed: action.payload}
+        case types.SET_SPLITS_INFO:
+            return {...state, splitsList: action.payload.list, splitsListTitle:action.payload.title}
         default:
             return state;
     }
